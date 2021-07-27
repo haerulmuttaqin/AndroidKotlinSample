786574ba07e20aea7d780b826810b5736b9aff1f
Haerul Muttaqin <haerulmuttaqin@gmail.com>
2021-07-27 14:06:16 +0700

    decrypt sqlite database

--- data/src/main/java/co/id/cpn/data/di/DatabaseModule.kt
+++ data/src/main/java/co/id/cpn/data/di/DatabaseModule.kt
@@ -2,12 +2,18 @@ package co.id.cpn.data.di
 
 import androidx.room.Room
 import co.id.cpn.data.local.InventoryRoomDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module { 
     single {
-        Room.databaseBuilder(androidContext(), InventoryRoomDatabase::class.java,"inventory_database").build()
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("cpn".toCharArray())
+        val factory = SupportFactory(passphrase)
+        Room.databaseBuilder(androidContext(), InventoryRoomDatabase::class.java,"inventory_database")
+            .openHelperFactory(factory)
+            .build()
     }
     single { 
         get<InventoryRoomDatabase>().inventoryDao()
